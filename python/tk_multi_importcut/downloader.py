cbd2e501917930a584ef68168940d8da7638ad9d
Raphael Matto <raphael.matto@autodesk.com>
2016-02-25 13:47:10 -0500

    For #34999, removed use of ThreadSafeShotgun in downloader.py. No remaining references to ThreadSafeShotgun.

--- python/tk_multi_importcut/downloader.py
+++ python/tk_multi_importcut/downloader.py
@@ -8,11 +8,14 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+import requests
+
 # by importing QT from sgtk rather than directly, we ensure that
 # the code will be compatible with both PySide and PyQt.
 from sgtk.platform.qt import QtCore
 import sgtk
-from .safe_shotgun import ThreadSafeShotgun
+
+
 class DownloadRunner(QtCore.QRunnable):
     """
     A runner to download things from Shotgun
@@ -32,8 +35,6 @@ class DownloadRunner(QtCore.QRunnable):
         :param path: Full file path to save the downloaded data
         """
         super(DownloadRunner, self).__init__()
-        # Build a thread safe Shotgun handle from the Toolkit one
-        self._sg = ThreadSafeShotgun(sgtk.platform.current_bundle().shotgun)
         self._sg_attachment = sg_attachment
         self._path = path
         self._thread = None
@@ -50,13 +51,15 @@ class DownloadRunner(QtCore.QRunnable):
         """
         Actually run the runner
         """
-        # Capture the thread we are running on
-        self._thread = QtCore.QThread.currentThread()
+        sg = sgtk.platform.current_bundle().shotgun
         try :
             if isinstance(self._sg_attachment, str):
-                self._sg.download_url(self._sg_attachment, self._path)
+                download_url(sg, self._sg_attachment, self._path)
             else:
-                attachment = self._sg.download_attachment(
+                # todo: test this code. But how do we create a situation
+                # where the thumbnail (is it always a thumbnail?) an
+                # attachment and not a string path to an aws bucket?
+                attachment = sg.download_attachment(
                     attachment=self._sg_attachment,
                     file_path=self._path)
             self._notifier.file_downloaded.emit(self._path)
@@ -64,3 +67,40 @@ class DownloadRunner(QtCore.QRunnable):
             raise
         finally:
             pass
+
+
+def download_url(self, url, location):
+    """
+    Downloads a file from a given url.
+    This method will take into account any proxy settings which have
+    been defined in the Shotgun connection parameters.
+    
+    :param url: url to download
+    :param location: path on disk where the payload should be written.
+                     this path needs to exists and the current user needs
+                     to have write permissions
+    :returns: nothing
+    """
+
+    proxies = {}
+    if self.config.proxy_server:
+        # handle proxy auth
+        if self.config.proxy_user and self.config.proxy_pass:
+            auth_string = "%s:%s@" % (self.config.proxy_user, self.config.proxy_pass)
+        else:
+            auth_string = ""
+        proxy_addr = "http://%s%s:%d" % (auth_string, self.config.proxy_server, self.config.proxy_port)
+        proxies["http"] = proxy_addr
+        proxies["https"] = proxy_addr
+    try:
+        response = requests.get(url, proxies=proxies)
+        response.raise_for_status()
+        # Write out the content into the given file
+        f = open(location, "wb")
+        try:
+            f.write(response.content)
+        finally:
+            f.close()
+    except Exception, e:
+        print "Could not download contents of url '%s'." % url
+        raise e
