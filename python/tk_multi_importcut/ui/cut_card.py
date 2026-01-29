694d18a05d026b272fada9e27ccfb2bd3ac933bb
Eduardo Chauca <166560435+eduardoChaucaGallegos@users.noreply.github.com>
2026-01-29 11:06:46 -0500

    removed old build_resources.sh (#81)

--- python/tk_multi_importcut/ui/cut_card.py
+++ python/tk_multi_importcut/ui/cut_card.py
@@ -1,98 +1,123 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'cut_card.ui'
-#
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'cut_card.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from tank.platform.qt import QtCore, QtGui
+from tank.platform.qt import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+from tank.platform.qt import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+
+from  . import resources_rc
 
 class Ui_CutCard(object):
     def setupUi(self, CutCard):
-        CutCard.setObjectName("CutCard")
+        if not CutCard.objectName():
+            CutCard.setObjectName(u"CutCard")
         CutCard.resize(433, 74)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.Preferred)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(CutCard.sizePolicy().hasHeightForWidth())
         CutCard.setSizePolicy(sizePolicy)
-        CutCard.setMinimumSize(QtCore.QSize(310, 70))
-        CutCard.setFocusPolicy(QtCore.Qt.StrongFocus)
-        CutCard.setStyleSheet("")
-        CutCard.setFrameShape(QtGui.QFrame.Box)
-        CutCard.setFrameShadow(QtGui.QFrame.Plain)
+        CutCard.setMinimumSize(QSize(310, 70))
+        CutCard.setFocusPolicy(Qt.StrongFocus)
+        CutCard.setStyleSheet(u"")
+        CutCard.setFrameShape(QFrame.Box)
+        CutCard.setFrameShadow(QFrame.Plain)
         CutCard.setLineWidth(2)
-        self.horizontalLayout = QtGui.QHBoxLayout(CutCard)
+        self.horizontalLayout = QHBoxLayout(CutCard)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(4, 1, 0, 1)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.icon_label = QtGui.QLabel(CutCard)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.icon_label.sizePolicy().hasHeightForWidth())
-        self.icon_label.setSizePolicy(sizePolicy)
-        self.icon_label.setMinimumSize(QtCore.QSize(105, 59))
-        self.icon_label.setMaximumSize(QtCore.QSize(105, 59))
-        self.icon_label.setBaseSize(QtCore.QSize(105, 59))
-        self.icon_label.setStyleSheet("background-color: black;")
-        self.icon_label.setText("")
-        self.icon_label.setPixmap(QtGui.QPixmap(":/tk_multi_importcut/sg_logo.png"))
+        self.icon_label = QLabel(CutCard)
+        self.icon_label.setObjectName(u"icon_label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.icon_label.sizePolicy().hasHeightForWidth())
+        self.icon_label.setSizePolicy(sizePolicy1)
+        self.icon_label.setMinimumSize(QSize(105, 59))
+        self.icon_label.setMaximumSize(QSize(105, 59))
+        self.icon_label.setBaseSize(QSize(105, 59))
+        self.icon_label.setStyleSheet(u"background-color: black;")
+        self.icon_label.setPixmap(QPixmap(u":/tk_multi_importcut/sg_logo.png"))
         self.icon_label.setScaledContents(False)
-        self.icon_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.icon_label.setObjectName("icon_label")
+        self.icon_label.setAlignment(Qt.AlignCenter)
+
         self.horizontalLayout.addWidget(self.icon_label)
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
+
+        self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setSpacing(2)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(-1, 4, -1, 4)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.title_label = QtGui.QLabel(CutCard)
-        self.title_label.setObjectName("title_label")
+        self.title_label = QLabel(CutCard)
+        self.title_label.setObjectName(u"title_label")
+
         self.verticalLayout_2.addWidget(self.title_label)
-        self.date_label = QtGui.QLabel(CutCard)
+
+        self.date_label = QLabel(CutCard)
+        self.date_label.setObjectName(u"date_label")
         self.date_label.setWordWrap(True)
-        self.date_label.setObjectName("date_label")
+
         self.verticalLayout_2.addWidget(self.date_label)
-        self.status_label = QtGui.QLabel(CutCard)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Preferred)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
+
+        self.status_label = QLabel(CutCard)
+        self.status_label.setObjectName(u"status_label")
         sizePolicy.setHeightForWidth(self.status_label.sizePolicy().hasHeightForWidth())
         self.status_label.setSizePolicy(sizePolicy)
         self.status_label.setWordWrap(True)
-        self.status_label.setObjectName("status_label")
+
         self.verticalLayout_2.addWidget(self.status_label)
-        self.details_label = QtGui.QLabel(CutCard)
-        self.details_label.setObjectName("details_label")
+
+        self.details_label = QLabel(CutCard)
+        self.details_label.setObjectName(u"details_label")
+
         self.verticalLayout_2.addWidget(self.details_label)
+
         self.horizontalLayout.addLayout(self.verticalLayout_2)
-        self.select_button = QtGui.QPushButton(CutCard)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.select_button.sizePolicy().hasHeightForWidth())
-        self.select_button.setSizePolicy(sizePolicy)
-        self.select_button.setMaximumSize(QtCore.QSize(30, 30))
-        self.select_button.setToolTip("")
-        self.select_button.setText("")
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/tk_multi_importcut/right_arrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
+        self.select_button = QPushButton(CutCard)
+        self.select_button.setObjectName(u"select_button")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.select_button.sizePolicy().hasHeightForWidth())
+        self.select_button.setSizePolicy(sizePolicy2)
+        self.select_button.setMaximumSize(QSize(30, 30))
+        icon = QIcon()
+        icon.addFile(u":/tk_multi_importcut/right_arrow.png", QSize(), QIcon.Normal, QIcon.Off)
         self.select_button.setIcon(icon)
-        self.select_button.setIconSize(QtCore.QSize(30, 30))
+        self.select_button.setIconSize(QSize(30, 30))
         self.select_button.setCheckable(False)
         self.select_button.setFlat(True)
-        self.select_button.setObjectName("select_button")
+
         self.horizontalLayout.addWidget(self.select_button)
+
         self.horizontalLayout.setStretch(1, 1)
 
         self.retranslateUi(CutCard)
-        QtCore.QMetaObject.connectSlotsByName(CutCard)
+
+        QMetaObject.connectSlotsByName(CutCard)
+    # setupUi
 
     def retranslateUi(self, CutCard):
-        CutCard.setWindowTitle(QtGui.QApplication.translate("CutCard", "Frame", None, QtGui.QApplication.UnicodeUTF8))
-        self.title_label.setText(QtGui.QApplication.translate("CutCard", "<big><b>Name</b></big>", None, QtGui.QApplication.UnicodeUTF8))
-        self.date_label.setText(QtGui.QApplication.translate("CutCard", "12/23/14 02:38 PM", None, QtGui.QApplication.UnicodeUTF8))
-        self.status_label.setText(QtGui.QApplication.translate("CutCard", "Status", None, QtGui.QApplication.UnicodeUTF8))
-        self.details_label.setText(QtGui.QApplication.translate("CutCard", "<small>details</small>", None, QtGui.QApplication.UnicodeUTF8))
-
-from . import resources_rc
+        CutCard.setWindowTitle(QCoreApplication.translate("CutCard", u"Frame", None))
+        self.icon_label.setText("")
+        self.title_label.setText(QCoreApplication.translate("CutCard", u"<big><b>Name</b></big>", None))
+        self.date_label.setText(QCoreApplication.translate("CutCard", u"12/23/14 02:38 PM", None))
+        self.status_label.setText(QCoreApplication.translate("CutCard", u"Status", None))
+        self.details_label.setText(QCoreApplication.translate("CutCard", u"<small>details</small>", None))
+#if QT_CONFIG(tooltip)
+        self.select_button.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+        self.select_button.setText("")
+    # retranslateUi
--- python/tk_multi_importcut/ui/entity_card.py
+++ python/tk_multi_importcut/ui/entity_card.py
@@ -1,90 +1,117 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'entity_card.ui'
-#
-#      by: pyside-uic 0.2.15 running on PySide 1.2.2
-#
-# WARNING! All changes made in this file will be lost!
+################################################################################
+## Form generated from reading UI file 'entity_card.ui'
+##
+## Created by: Qt User Interface Compiler version 5.15.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
 
-from tank.platform.qt import QtCore, QtGui
+from tank.platform.qt import QtCore
+for name, cls in QtCore.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+from tank.platform.qt import QtGui
+for name, cls in QtGui.__dict__.items():
+    if isinstance(cls, type): globals()[name] = cls
+
+
+from ..dialog import ElidedLabel
+
+from  . import resources_rc
 
 class Ui_EntityCard(object):
     def setupUi(self, EntityCard):
-        EntityCard.setObjectName("EntityCard")
+        if not EntityCard.objectName():
+            EntityCard.setObjectName(u"EntityCard")
         EntityCard.resize(358, 70)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.MinimumExpanding)
+        sizePolicy = QSizePolicy(QSizePolicy.Preferred, QSizePolicy.MinimumExpanding)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(EntityCard.sizePolicy().hasHeightForWidth())
         EntityCard.setSizePolicy(sizePolicy)
-        EntityCard.setMinimumSize(QtCore.QSize(310, 70))
-        EntityCard.setMaximumSize(QtCore.QSize(470, 16777215))
-        EntityCard.setBaseSize(QtCore.QSize(358, 70))
-        EntityCard.setFocusPolicy(QtCore.Qt.StrongFocus)
-        EntityCard.setStyleSheet("")
-        EntityCard.setFrameShape(QtGui.QFrame.Box)
-        EntityCard.setFrameShadow(QtGui.QFrame.Plain)
+        EntityCard.setMinimumSize(QSize(310, 70))
+        EntityCard.setMaximumSize(QSize(470, 16777215))
+        EntityCard.setBaseSize(QSize(358, 70))
+        EntityCard.setFocusPolicy(Qt.StrongFocus)
+        EntityCard.setStyleSheet(u"")
+        EntityCard.setFrameShape(QFrame.Box)
+        EntityCard.setFrameShadow(QFrame.Plain)
         EntityCard.setLineWidth(2)
-        self.horizontalLayout = QtGui.QHBoxLayout(EntityCard)
+        self.horizontalLayout = QHBoxLayout(EntityCard)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
         self.horizontalLayout.setContentsMargins(4, 1, 0, 1)
-        self.horizontalLayout.setObjectName("horizontalLayout")
-        self.icon_label = QtGui.QLabel(EntityCard)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Fixed)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.icon_label.sizePolicy().hasHeightForWidth())
-        self.icon_label.setSizePolicy(sizePolicy)
-        self.icon_label.setMinimumSize(QtCore.QSize(105, 59))
-        self.icon_label.setMaximumSize(QtCore.QSize(105, 59))
-        self.icon_label.setBaseSize(QtCore.QSize(105, 59))
-        self.icon_label.setStyleSheet("background-color: black;")
-        self.icon_label.setText("")
-        self.icon_label.setPixmap(QtGui.QPixmap(":/tk_multi_importcut/default_card_icon.png"))
+        self.icon_label = QLabel(EntityCard)
+        self.icon_label.setObjectName(u"icon_label")
+        sizePolicy1 = QSizePolicy(QSizePolicy.Fixed, QSizePolicy.Fixed)
+        sizePolicy1.setHorizontalStretch(0)
+        sizePolicy1.setVerticalStretch(0)
+        sizePolicy1.setHeightForWidth(self.icon_label.sizePolicy().hasHeightForWidth())
+        self.icon_label.setSizePolicy(sizePolicy1)
+        self.icon_label.setMinimumSize(QSize(105, 59))
+        self.icon_label.setMaximumSize(QSize(105, 59))
+        self.icon_label.setBaseSize(QSize(105, 59))
+        self.icon_label.setStyleSheet(u"background-color: black;")
+        self.icon_label.setPixmap(QPixmap(u":/tk_multi_importcut/default_card_icon.png"))
         self.icon_label.setScaledContents(False)
-        self.icon_label.setAlignment(QtCore.Qt.AlignCenter)
-        self.icon_label.setObjectName("icon_label")
+        self.icon_label.setAlignment(Qt.AlignCenter)
+
         self.horizontalLayout.addWidget(self.icon_label)
-        self.verticalLayout_2 = QtGui.QVBoxLayout()
+
+        self.verticalLayout_2 = QVBoxLayout()
         self.verticalLayout_2.setSpacing(1)
+        self.verticalLayout_2.setObjectName(u"verticalLayout_2")
         self.verticalLayout_2.setContentsMargins(-1, 4, -1, 4)
-        self.verticalLayout_2.setObjectName("verticalLayout_2")
-        self.title_label = QtGui.QLabel(EntityCard)
-        self.title_label.setObjectName("title_label")
+        self.title_label = QLabel(EntityCard)
+        self.title_label.setObjectName(u"title_label")
+
         self.verticalLayout_2.addWidget(self.title_label)
-        self.status_label = QtGui.QLabel(EntityCard)
-        self.status_label.setObjectName("status_label")
+
+        self.status_label = QLabel(EntityCard)
+        self.status_label.setObjectName(u"status_label")
+
         self.verticalLayout_2.addWidget(self.status_label)
+
         self.details_label = ElidedLabel(EntityCard)
-        self.details_label.setObjectName("details_label")
+        self.details_label.setObjectName(u"details_label")
+
         self.verticalLayout_2.addWidget(self.details_label)
+
         self.horizontalLayout.addLayout(self.verticalLayout_2)
-        self.select_button = QtGui.QPushButton(EntityCard)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Maximum, QtGui.QSizePolicy.Maximum)
-        sizePolicy.setHorizontalStretch(0)
-        sizePolicy.setVerticalStretch(0)
-        sizePolicy.setHeightForWidth(self.select_button.sizePolicy().hasHeightForWidth())
-        self.select_button.setSizePolicy(sizePolicy)
-        self.select_button.setMaximumSize(QtCore.QSize(30, 30))
-        self.select_button.setToolTip("")
-        self.select_button.setText("")
-        icon = QtGui.QIcon()
-        icon.addPixmap(QtGui.QPixmap(":/tk_multi_importcut/right_arrow.png"), QtGui.QIcon.Normal, QtGui.QIcon.Off)
+
+        self.select_button = QPushButton(EntityCard)
+        self.select_button.setObjectName(u"select_button")
+        sizePolicy2 = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        sizePolicy2.setHorizontalStretch(0)
+        sizePolicy2.setVerticalStretch(0)
+        sizePolicy2.setHeightForWidth(self.select_button.sizePolicy().hasHeightForWidth())
+        self.select_button.setSizePolicy(sizePolicy2)
+        self.select_button.setMaximumSize(QSize(30, 30))
+        icon = QIcon()
+        icon.addFile(u":/tk_multi_importcut/right_arrow.png", QSize(), QIcon.Normal, QIcon.Off)
         self.select_button.setIcon(icon)
-        self.select_button.setIconSize(QtCore.QSize(30, 30))
+        self.select_button.setIconSize(QSize(30, 30))
         self.select_button.setCheckable(False)
         self.select_button.setFlat(True)
-        self.select_button.setObjectName("select_button")
+
         self.horizontalLayout.addWidget(self.select_button)
+
         self.horizontalLayout.setStretch(1, 1)
 
         self.retranslateUi(EntityCard)
-        QtCore.QMetaObject.connectSlotsByName(EntityCard)
+
+        QMetaObject.connectSlotsByName(EntityCard)
+    # setupUi
 
     def retranslateUi(self, EntityCard):
-        EntityCard.setWindowTitle(QtGui.QApplication.translate("EntityCard", "Frame", None, QtGui.QApplication.UnicodeUTF8))
-        self.title_label.setText(QtGui.QApplication.translate("EntityCard", "<big><b>Name</b></big>", None, QtGui.QApplication.UnicodeUTF8))
-        self.status_label.setText(QtGui.QApplication.translate("EntityCard", "Status", None, QtGui.QApplication.UnicodeUTF8))
-        self.details_label.setText(QtGui.QApplication.translate("EntityCard", "<small>details</small>", None, QtGui.QApplication.UnicodeUTF8))
-
-from ..dialog import ElidedLabel
-from . import resources_rc
+        EntityCard.setWindowTitle(QCoreApplication.translate("EntityCard", u"Frame", None))
+        self.icon_label.setText("")
+        self.title_label.setText(QCoreApplication.translate("EntityCard", u"<big><b>Name</b></big>", None))
+        self.status_label.setText(QCoreApplication.translate("EntityCard", u"Status", None))
+        self.details_label.setText(QCoreApplication.translate("EntityCard", u"<small>details</small>", None))
+#if QT_CONFIG(tooltip)
+        self.select_button.setToolTip("")
+#endif // QT_CONFIG(tooltip)
+        self.select_button.setText("")
+    # retranslateUi
